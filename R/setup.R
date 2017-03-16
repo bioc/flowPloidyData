b843fcb4cb54147c0377f0bb8129416a4972018c
Tyler Smith <tyler@plantarum.ca>
2017-03-16 19:17:55 +0000

    Added fpVac for access to new data
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/flowPloidyData@4082 db2202da-8704-0410-b924-b125b3cd84ef

--- R/setup.R
+++ R/setup.R
@@ -2,3 +2,7 @@ flowPloidyFiles <-
   list.files(system.file("extdata/", package = "flowPloidyData"), 
              pattern = "*.LMD", full.names = TRUE)
 names(flowPloidyFiles) <- basename(flowPloidyFiles)
+
+fpVac <- list.files(system.file("extdata/",
+                                package = "flowPloidyData"), 
+                    pattern = "vac", full.names = TRUE)
