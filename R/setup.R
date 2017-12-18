b12a5aab3e79855879f8ffa9144bc7e3dc82afb9
Tyler Smith <tyler@plantarum.ca>
2017-12-18 07:32:47 -0500

    Added additional data file for testing, bumped version number

--- R/setup.R
+++ R/setup.R
@@ -6,3 +6,7 @@ names(flowPloidyFiles) <- basename(flowPloidyFiles)
 fpVac <- list.files(system.file("extdata/",
                                 package = "flowPloidyData"), 
                     pattern = "vac", full.names = TRUE)
+
+fpBad <- list.files(system.file("extdata/",
+                                package = "flowPloidyData"),
+                    pattern = "08.328", full.names = TRUE)
